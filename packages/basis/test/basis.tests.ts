9edd708efffe85dd25ac924864994c3b3b6a9153
Kristof Van Der Haeghen <k.van.der.haeghen.aleacsys@napoleongames.be>
2023-12-02 14:14:27 +0100

    Fixing basis & ktx2 tests.

--- packages/basis/test/basis.tests.ts
+++ packages/basis/test/basis.tests.ts
@@ -1,22 +1,23 @@
+/* eslint-disable max-len */
 import { BasisParser, loadBasis } from '@pixi/basis';
-import { path } from '@pixi/utils';
 import { Loader } from '../../assets/src/loader/Loader';
 
 import type { Texture } from '@pixi/core';
+
+const loaderJS = 'https://raw.githubusercontent.com/BinomialLLC/basis_universal/master/webgl/transcoder/build/basis_transcoder.js';
+const loaderWASM = 'https://raw.githubusercontent.com/BinomialLLC/basis_universal/master/webgl/transcoder/build/basis_transcoder.wasm';
+const basisTexture = 'https://github.com/Sparcks/pixijs/raw/feature/load-ktx2/packages/basis/test/images/kodim20.basis';
 
 describe('Basis loading', () =>
 {
     it('should load a a Basis image', async () =>
     {
-        await BasisParser.loadTranscoder(
-            path.join(__dirname, '../assets/basis_transcoder.js'),
-            path.join(__dirname, '../assets/basis_transcoder.wasm')
-        );
+        await BasisParser.loadTranscoder(loaderJS, loaderWASM);
 
         const loader = new Loader();
 
         loader['_parsers'].push(loadBasis);
-        const texture = await loader.load<Texture>(path.join(__dirname, './images/kodim20.basis'));
+        const texture = await loader.load<Texture>(basisTexture);
 
         expect(texture.baseTexture.valid).toBe(true);
         expect(texture.width).toBe(768);
